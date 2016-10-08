bd024d45152b02fb27531fb5745306faad71ff81
BuildTools <unconfigured@null.spigotmc.org>
2016-10-08 10:02:57 +0100

    Updated with my code

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,52 +1,71 @@
-""" Hangman Game (v1.0)
-    Name: 
-    Date: 
-    """
-
-# imported modules go here
-
-def load_file(filename):
-    """ Function to return a word list from a plain text file;
-        Note: You will need to open the file, read and append
-        each line to an array (or list), close the file and
-        then return the array.
-        """
-    word_list = []
-    # your code goes here
-    return word_list
-
-def select_word(word_list):
-    """ Function to return a random word from an array of words;
-        Note: You will need to import the random module and use
-        random.randint() to select a random index in the array.
-        """
-    # your code goes here
-    return word
-
-def find_character(char, word):
-    """ Function to return the indices of character in word;
-        Note: This should return an array of indices containing:
-        1 index if the character appears only once, multiple indices if 
-        the character appears more than once and an empty array if the
-        character is not found.
-        """
-    # your code goes here
-    return char_pos
-
-# do you need any other functions or procedures?
-
+#Hangman Game (v2.0) Name: Joe Young Date: 24/09/2016 minimin about of lines
+import sys
+sys.path.append(sys.path[0]+'\\Extra')
+from random import *
+from time import *
+import hangmanp
+def random_word(filename): #Opens file, gets sets to word list and returns a random word
+    file = open (filename) 
+    word_list = file.readlines() 
+    file.close()
+    single_word = (word_list[randint(0, len(word_list)-1)]) 
+    return single_word
+def play_again():
+    while 0 != 1:
+        again = str(input("Would you like to play again?\n-")).lower()
+        if again == "yes":
+            print("You have choosen to play again!\n")
+            sleep(1)
+            return main()
+        elif again == "no":
+            print("You have choosen to not play again!")
+            sleep(3)
+            break
+        else:
+            print("Please input a vaild method!\n")
 def main():
-    """ Note: This is your main function and should contain your game loop.
-        """
-    filename = "word_list.txt"
-    attempts_remaining = 10
-    # do you need any other variables?
+    ran_word = random_word(sys.path[0]+'\\Extra\wordlists.txt')
+    li = (len(ran_word)-1)
+    left = ["_" for i in range (li)]
+    trys = 0
+    guess = 0
+    print(ran_word)
+    while 0 != 1:
+        for place in left:
+            print(place, end=" ")
+        print ("\nThe Word has ", li, " letters! \nGuess = ", trys )
+        letter = str(input("What is your guess?\n-")).lower()
+        if ran_word.find(letter) == -1:
+            print ("Your guess was incorrect\n")
+            if guess != 11:
+                print (hangmanp.hangman(guess))
+                guess += 1
+        else:
+            if not(len(letter) == 1):
+                print("Please only entre one letter at a time!\n")
+            else:
+                if not((letter) in left):
+                    print("Your guess was correct!\n")
+                else:
+                    print("You have already correctly guessed", letter, "!\n")
+            
+            for i in range(li):
+                if ran_word[i] == letter:
+                    left[i] = letter
 
-    word_list = load_file(filename)
-    word = select_word(word_list)
-    
-    # your code goes here
-
-
+            if not(("_") in left): 
+                for place in left:
+                    print(place, end=" ")
+                print("\nYou have won the game!, it took you ", trys, " guesses!\n")
+                sleep(3)
+                return play_again()
+                break
+        trys += 1
+        if not(guess != 11):
+                print("You have exceed your guesses!\n")
+                print("You have lost the game!, it took you ", trys, "trys to fail :(\n")
+                sleep(5)
+                return play_again()
+                break
 if __name__ == "__main__":
     main()
