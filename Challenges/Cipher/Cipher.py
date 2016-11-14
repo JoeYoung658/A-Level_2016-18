10a670346d44ed29230efc0558deb0299df80295
Joe Young <josephyoung658@gmail.com>
2016-11-14 19:41:02 +0000

    UnicodeEncodeError

--- Challenges/Cipher/Cipher.py
+++ Challenges/Cipher/Cipher.py
@@ -8,7 +8,7 @@ import sys
 def convert_to_ascii(string):
     text = []
     for char in string:
-        text.append(str(ord(char)))
+        text.append(int(ord(char)))
     return text
 
 def convert_to_word(ascii_list):
@@ -29,7 +29,7 @@ def decrease_ascii(ascii_list, amount):
 
 
 import random
-passwd  = "Hello!!!!!!!!!!!!!!!!!!!!!!!"
+passwd  = "Hi"
 n = 1
 #ran = random.Random(passwd).sample(range(1000), n)
 ran = random.Random(passwd).randint(0, 1000)
@@ -37,39 +37,52 @@ print(ran)
 
 print("--------------------------------")
 
-user_input = "Hello my name is Joe!"
+#user_input = "Hello my name is Joe!"
+# file = open(sys.path[0]+'\\Extra\Input.txt', 'r')
+# user_input = file.read()
+# file.close
+with open(sys.path[0]+'\\Extra\Input.txt') as myfile:
+    user_input="".join(line.rstrip() for line in myfile)
+
+print(user_input)
 
 ascii_list = convert_to_ascii(user_input)
+#print(ascii_list)
 
-sentance  = convert_to_word(ascii_list)
+Oringal_Sentace  = convert_to_word(ascii_list)
+#print(Oringal_Sentace)
 
-increased_ascii_list = increase_ascii([int(i) for i in ascii_list], ran)
+increased_ascii_list = increase_ascii(ascii_list, ran)
+#print(increased_ascii_list)
 
-new_ascii_list = [str(i) for i in increased_ascii_list]
+cipher = convert_to_word(increased_ascii_list)
+print(str(cipher))
+# cipher2 = cipher
+decreased_ascii_list = decrease_ascii(increased_ascii_list, ran)
+#print(decreased_ascii_list)
 
-increased_sentance  = convert_to_word(new_ascii_list)
-
-decrease_sentance = decrease_ascii(increased_ascii_list, ran)
-
-new_sentance = convert_to_word(decrease_sentance)
-
-print(ascii_list)
-print(sentance)
-print(increased_ascii_list)
-print(new_ascii_list)
-print(increased_sentance)
-print(decrease_sentance)
-print(new_sentance)
+uncipher = convert_to_word(decreased_ascii_list)
+print(uncipher)
 
 
+file = open(sys.path[0]+'\\Extra\Output.txt', 'a')
+file.write(str(cipher))
+file.close
 
 
-
-
+print("Done")
 
 
 
 
+
+
+
+
+
+#increased_ascii_list = increase_ascii([int(i) for i in ascii_list], ran)
+#string_ascii_list = [str(i) for i in increased_ascii_list]
+#print(string_ascii_list)
 #print(ord(user_input))
 
 ##text = ""
