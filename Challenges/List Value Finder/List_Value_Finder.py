cb290e817f179ca6840e263a22133ada4d994cce
joeyoung658 <josephyoung658@gmail.com>
2016-12-09 09:51:50 +0000

    Add files via upload

--- Challenges/List Value Finder/List_Value_Finder.py
+++ Challenges/List Value Finder/List_Value_Finder.py
@@ -15,24 +15,14 @@ def value_finder_r_single(List):
     else:
         return (List[0] + value_finder_r_single(List[1:]))
 
-def value_finder_multi(List):
+def value_finder_r_multi(List):
     value = 0    
     for i in List:
         if type(i) == list:
-            value += value_finder(i)
+            value += value_finder_multi(i)
         else:
             value += i
     return value
-
-
-def value_finder_r_multi(List):
-    if List == []:
-        return 0
-    else:
-        if type(List[0]) == list:
-            return (List[0][0] + value_finder_r_multi(List[0][1:]))
-        else:
-            return (List[0] + value_finder_r_multi(List[1:]))
 
 
 def main():
@@ -44,13 +34,10 @@ def main():
     two = value_finder_r_single(lists)
     print(two)
     #----------------------------------------
-    lists = [10,10,[5,5]]
+    lists = [10,10,[5,5],5,5,[5,[5,5],5]]
     three = value_finder_multi(lists)
     print(three)
-    #----------------------------------------
-    lists = [10,10,[5,5,[5,5,[5,5,[5,5,[5,5]]]]]]
-    four = value_finder_r_multi(lists)
-    print(four)
+    
 
 
 if __name__ == "__main__":
