68672091fba216a29057761ce243d1e51f746113
Joe Young <josephyoung658@gmail.com>
2016-12-30 14:45:25 +0000

    Snap

--- Challenges/Card Game/Snap.py
+++ Challenges/Card Game/Snap.py
@@ -1,4 +1,6 @@
 """ Classes. V.1 Joe Young """
+import random
+import time
 
 RANKS = { 1  :  "Ace",
           2  :  "2",
@@ -9,9 +11,10 @@ RANKS = { 1  :  "Ace",
           7  :  "7",
           8  :  "8",
           9  :  "9",
-          10 :  "J",
-          11 :  "Q",
-          12 :  "K"}
+          10 :  "10",
+          11 :  "J",
+          12 :  "Q",
+          13 :  "K"}
 
 SUITS = { 1 : "Hearts",
           2 : "Spades",
@@ -22,11 +25,17 @@ SUITS = { 1 : "Hearts",
 class card():
     global RANKS
     global SUITS
+    
+    tdeck = []
+
     def __init__(self, rank, suit):
-        rank = (RANKS[rank])
-        suit = (SUITS[suit])
+        # rank = (RANKS[rank])
+        # suit = (SUITS[suit])
         self.rank = rank
         self.suit = suit
+        
+    def number_to_word(self):
+        return (self.rank + self.suit)
 
     def print_card(self):
         print("Rank:", self.rank)
@@ -34,12 +43,21 @@ class card():
 
 
 def main():
+    global card
     Deck = []
-    for suit in range(1, 4):
-        for rank in range(1, 13):
-            Deck.append(card(rank, suit))   
+    tDeck = []
+    for suit in range(1, 5):
+        for rank in range(1, 14):
+            Deck.append(card(rank, suit))
+
     for card in Deck:
-        print(card.rank, card.suit)
+        tDeck.append((card.rank, card.suit))
+
+    print(tDeck)
+    print(len(Deck))
+    random.shuffle(tDeck)
+    print(tDeck)
+
 
 if __name__ == "__main__":
     main()
