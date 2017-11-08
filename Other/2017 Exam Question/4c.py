55dae77bd8501e74355f51fce08a6685fa7009f2
Joe Young <josephyoung658@gmail.com>
2017-11-08 19:42:35 +0000

    4C

---
+++ Other/2017 Exam Question/4c.py
@@ -0,0 +1,15 @@
+l = [[1,5,7,12],
+    [3,4,15,16],
+    [0,0,1,3],
+    [12,16,18,23]]
+
+
+
+
+for j in range(len(l)):
+    total = 0
+    mean = 0 
+    for i in range(len(l)):
+       total += l[i][j]
+    print(total/len(l))
+    
